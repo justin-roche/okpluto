b7b947e1b15ed676ecf80649331b62be53789566
Daisy Good <daisy.m.good@gmail.com>
2016-10-27 17:54:54 -0500

    Add mongo promise

--- config/db.js
+++ config/db.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/okplutodb');
 
 var db = mongoose.connection;
