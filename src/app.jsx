6258123c9d96f676c65b832dad58c508fb585d2c
Daisy Good <daisy.m.good@gmail.com>
2016-10-29 20:42:29 -0500

    fix domain typo

--- src/app.jsx
+++ src/app.jsx
@@ -23,7 +23,7 @@ import InfoDrawer from './components/infoDrawer.jsx'
 
 getInfo().then(authPath => {
   console.log(authPath);
-const auth = new AuthService(authPath.auth.id, authPath.auth.authDomain);
+const auth = new AuthService(authPath.auth.id, authPath.auth.domain);
 
 const requireAuth = (nextState, replace) => {
   if (!auth.loggedIn()) {
